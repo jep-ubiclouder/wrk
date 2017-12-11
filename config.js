608b19bb2367f1620e0ceb3813c27fc87b14b24d
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-12-11 13:09:22 +0100

    mot de passe

--- config.js
+++ config.js
@@ -8,9 +8,15 @@ exports.PUSH_TOPIC = 'AccountsTopic';
 exports.CLIENT_SECRET = "6123002037877937632"; // Consumer Secret
 exports.USERNAME = "jean.eric.preis@gmail.com"; // Salesforce Login Username
 exports.PASSWORD = "hecate1706ReCA04erotQxPp6odiyAYzZs" // Salesforce Password + Token (i.e if abcd is the password and xxxxxx is the security token then this parameter would be: abcdxxxxxx)
-*/
 
-exports.CLIENT_ID = "3MVG96mGXeuuwTZgfJEN0oc1D0ROomSShKOrV0jkgeNy1l1X8.sMB_24GlyrtFTY9YYNQeVSo3kmw4zBqp_Xg"; // Consumer Key
+
+exports.CLIENT_ID = "3sMVG96mGXeuuwTZgfJEN0oc1D0ROomSShKOrV0jkgeNy1l1X8.sMB_24GlyrtFTY9YYNQeVSo3kmw4zBqp_Xg"; // Consumer Key
 exports.CLIENT_SECRET = "8582528312076741407"; // Consumer Secret
 exports.USERNAME = "jep@asembtest.com"; // Salesforce Login Username
 exports.PASSWORD = "ubi$2016fhgTM7HaaZYThAjZJA4u8FbgO" // Salesforce Password + Token (i.e if abcd is the password and xxxxxx is the security token then this parameter would be: abcdxxxxxx)
+
+*/
+exports.CLIENT_ID = "3MVG9w8uXui2aB_q3bYH2D61uCs6aO2mutGdS7FVCDvOA266tMtmrtRFawy9XQnSjt5cxtREOZQz49J4J4Qs5"; // Consumer Key
+exports.CLIENT_SECRET = "5710322776849835493"; // Consumer Secret
+exports.USERNAME = "jep@ubiclouder.com.mm"; // Salesforce Login Username
+exports.PASSWORD ="ubi$2017Xqr6eOjUX2MrVzoZfKg7Iull" // Salesforce Password + Token (i.e if abcd is the password and xxxxxx is the security token then this parameter would be: abcdxxxxxx)
