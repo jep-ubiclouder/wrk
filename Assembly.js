1af3febc900510d0f7c318a2aa9df2ef247adc36
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-12-13 14:49:52 +0100

    async

--- Assembly.js
+++ Assembly.js
@@ -126,7 +126,8 @@ function mapRT(org,oauth){
 }
 const  getLT = async (org,oauth,field,myId) => {
 	var q = "select id,"+field+"  from Biography__c where Id='"+myId +"'";
-	var b = 1
+	var b = 1;
+	console.log('in getLT',q);
 	let resultSQL
 	try{
 		resultSQL = await  org.query({
