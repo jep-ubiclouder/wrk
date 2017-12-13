6aa39938eebe7d9d81dd4e32bdddd5fa133ec123
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-12-13 13:41:07 +0100

    promise

--- Assembly.js
+++ Assembly.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-
+var Promise = require('promise');
 
 var config = require('./config.js');
 // plugin SF
