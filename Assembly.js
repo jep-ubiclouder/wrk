5ad484e7b0e52c84ae4430ca9895778c5cda84ff
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-12-13 11:40:10 +0100

    return b

--- Assembly.js
+++ Assembly.js
@@ -134,15 +134,17 @@ const getLTF= function(org,oauth,field,myId){
     } , function(err,resp){
         var b = {'field':field, 'value': resp.records[0].get(field)};
         console.log(b);
+        
         var bio = nforce.createSObject('Biography__c');
         bio.set('Id',myId);
         bio.set('chkBioRTF__c',false);
+        
         org.update({sobject:bio, oauth:oauth}, function(err, r){
         	  if(!err) console.log('It worked!');
         });
-        
+        return b
     })
-    return b
+
 }
 
 org.authenticate({
