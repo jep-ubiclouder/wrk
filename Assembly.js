d4e150682a10c2f1a728ffff9d79a843764e1f1f
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-12-13 14:52:08 +0100

    yo

--- Assembly.js
+++ Assembly.js
@@ -128,23 +128,17 @@ const  getLT = async (org,oauth,field,myId) => {
 	var q = "select id,"+field+"  from Biography__c where Id='"+myId +"'";
 	var b = 1;
 	console.log('in getLT',q);
-	let resultSQL
-	try{
-		resultSQL = await  org.query({
+	let resultSQL = await  org.query({
 	        oauth:oauth,
 	        query : q
 	    }	, function(err,resp){
 	    	
 	    	if (err) return err;
-	        var b = {'field':field, 'value': resp.records[0].get(field)};
+	        b = {'field':field, 'value': resp.records[0].get(field)};
 	        // console.log(b);	        
 	        }
 		);
-		return b;
-	}
-	catch (err){
-		console.log(err);
-	} 
+		
 	console.log('resultSQL',resultSQL);
 	console.log('b',b);
 	return resultSQL
