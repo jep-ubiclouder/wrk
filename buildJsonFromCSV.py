505927fbe85d67b635fd55880c8619e810178cfb
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2018-02-14 16:52:18 +0100

    first commit

--- buildJsonFromCSV.py
+++ buildJsonFromCSV.py
@@ -1,3 +1,10 @@
 import csv
 import JSON
 import sys
+
+
+if __name__ =='__main__':
+    with open('./categorie.csv','r') as f:
+        reader = csv.DictReader(f, delimiter=';')
+        for l in reader:
+            print(l)
