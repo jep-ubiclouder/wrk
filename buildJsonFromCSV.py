dddd8d39378ffef4e9000c1c4b026b124c681bcd
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2018-02-14 17:04:23 +0100

    plop

--- buildJsonFromCSV.py
+++ buildJsonFromCSV.py
@@ -4,7 +4,17 @@ import sys
 
 
 if __name__ =='__main__':
+    arbre = {'0':{'Name':'root','children':[]}
     with open('./categorie.csv','r') as f:
         reader = csv.DictReader(f, delimiter=';')
         for l in reader:
-            print(l)
+            if l['id'] not in arbre.keys():
+                arbre[l['id']] = {'Name':l['Nom'],'children':[]}
+            else:
+                arbre[l['id']]['Name'] = l['Nom']
+            if l['Parent'] not in arbre.keys():
+                arbre[l['Parent']]  ={'children':[]}
+                
+            arbre[l['Parent']]['children'].push(l['id'])
+            
+    print(arbre)            
