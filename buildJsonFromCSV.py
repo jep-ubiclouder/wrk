aabd5f3ea0aeb19f206686f1c1ac510bdb195a1d
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2018-02-15 09:26:55 +0100

    go

--- buildJsonFromCSV.py
+++ buildJsonFromCSV.py
@@ -16,22 +16,7 @@ from simple_salesforce import (
 
 
 if __name__ =='__main__':
-	"""    arbre = {'0':{'Name':'root','children':[]}}
-    with open('./categorie.csv','r') as f:
-        reader = csv.DictReader(f, delimiter=';')
-        for l in reader:
-            if l['id'] not in arbre.keys():
-                arbre[l['id']] = {'Name':l['Nom'],'children':[]}
-            else:
-                arbre[l['id']]['Name'] = l['Nom']
-            if l['Parent'] not in arbre.keys():
-                arbre[l['Parent']]  ={'children':[]}
-                
-            arbre[l['Parent']]['children'].append(l['id'])
-            
-    print(arbre)            
-    """
-    
+   
     sf = Salesforce(username='jep@ubiclouder.com.mm', password='ubi$2017', security_token='Xqr6eOjUX2MrVzoZfKg7Iull', sandbox=True)
     toInsert =[]   ## On utilise l'API bulk
     
