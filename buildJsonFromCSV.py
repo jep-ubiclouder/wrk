4d9df91da09393e23d67998ab10218aebe0c6281
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2018-02-15 09:15:02 +0100

    insert categories

--- buildJsonFromCSV.py
+++ buildJsonFromCSV.py
@@ -1,10 +1,22 @@
 import csv
-
 import sys
+from simple_salesforce import (
+    Salesforce,
+    SalesforceAPI,
+    SFType,
+    SalesforceError,
+    SalesforceMoreThanOneRecord,
+    SalesforceExpiredSession,
+    SalesforceRefusedRequest,
+    SalesforceResourceNotFound,
+    SalesforceGeneralError,
+    SalesforceMalformedRequest
+)
+
 
 
 if __name__ =='__main__':
-    arbre = {'0':{'Name':'root','children':[]}}
+"""    arbre = {'0':{'Name':'root','children':[]}}
     with open('./categorie.csv','r') as f:
         reader = csv.DictReader(f, delimiter=';')
         for l in reader:
@@ -18,3 +30,17 @@ if __name__ =='__main__':
             arbre[l['Parent']]['children'].append(l['id'])
             
     print(arbre)            
+    """
+    
+    sf = Salesforce(username='jep@ubiclouder.com.mm', password='ubi$2017', security_token='Xqr6eOjUX2MrVzoZfKg7Iull', sandbox=True)
+    toInsert =[]   ## On utilise l'API bulk
+    
+    with open('./categorie.csv','r') as f:
+        reader = csv.DictReader(f, delimiter=';')
+        for l in reader:
+            newRec= {'Clef_Statec__c':l['id'],'Clef_Parent__c':l['Parent'],'Name':l['Nom'],'Libelle__c':l['Nom'],'Poids__c':l['Poids']}
+            toInsert.append(newRec)
+            
+    sf.Business_Category__c.bulk.insert(toInsert)
+    
+    
